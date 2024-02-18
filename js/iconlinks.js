c86169cd8e1ea24bc84df5344613b95f9f9d1e15
jordanxlau <jordanxlau@yahoo.ca>
2024-02-18 13:49:04 -0500

    fixed bug where footer icons did not link

---
+++ js/iconlinks.js
@@ -0,0 +1,18 @@
+//Doing anything else in HTML messes up the display of the images
+$(function(){
+    $("#linkedinbutton").click(function(){
+        window.location.href = "https://www.linkedin.com/in/jordan-lau-40454b233";
+    });
+});
+
+$(function(){
+    $("#youtubebutton").click(function(){
+        window.location.href = "https://www.youtube.com/channel/UC2FWBp83ePz_3naCMxBYbng";
+    });
+});
+
+$(function(){
+    $("#githubbutton").click(function(){
+        window.location.href = "https://github.com/jordanxlau";
+    });
+});
