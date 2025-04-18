b3d3a0993fe35261a1266c01902a671cfab73e6b
jordanxlau <jordanxlau@yahoo.ca>
2025-04-17 22:13:22 -0400

    bugfix: fixed more linting warnings

--- src/Footer.js
+++ src/Footer.js
@@ -2,11 +2,11 @@ function Footer() {
     return (
       <footer class="container-fluid p-3 text-white bg-primary text-right">
         <div class = "row p-2" style = {{marginLeft: 40}}>
-            <a class = "col text-left">
+            <p class = "col text-left">
                 <i id = "linkedinbutton" class="fab fa-linkedin" style={{color:'#FFFFFF'}}></i>
                 <i id = "youtubebutton" class="fab fa-youtube" style={{color: '#FFFFFF'}}></i>
                 <i id = "githubbutton" class="fab fa-github" style={{color: '#FFFFFF'}}></i>
-            </a>
+            </p>
         </div>
         <div class = "row p-2" style = {{marginLeft: 40}}>
             <p class = "col text-left"> Toronto | Ottawa | jordanxlau@yahoo.ca | &copy;2025 Jordan Lau </p>
