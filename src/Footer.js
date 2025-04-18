40b9d410eb6ad3438d01164295f9ad7583a7dcc6
jordanxlau <jordanxlau@yahoo.ca>
2025-04-18 00:07:04 -0400

    bugfix: footer icon links work

--- src/Footer.js
+++ src/Footer.js
@@ -3,9 +3,9 @@ function Footer() {
       <footer class="container-fluid p-3 text-white bg-primary text-right">
         <div class = "row p-2" style = {{marginLeft: 40}}>
             <p class = "col text-left">
-                <i id = "linkedinbutton" class="fab fa-linkedin" style={{color:'#FFFFFF'}}></i>
-                <i id = "youtubebutton" class="fab fa-youtube" style={{color: '#FFFFFF'}}></i>
-                <i id = "githubbutton" class="fab fa-github" style={{color: '#FFFFFF'}}></i>
+                <a href = "https://www.linkedin.com/in/jordan-lau-40454b233" class="fab fa-linkedin" style={{color:'#FFFFFF'}}></a>
+                <a href = "https://www.youtube.com/channel/UC2FWBp83ePz_3naCMxBYbng" class="fab fa-youtube" style={{color: '#FFFFFF'}}></a>
+                <a href = "https://github.com/jordanxlau" class="fab fa-github" style={{color: '#FFFFFF'}}></a>
             </p>
         </div>
         <div class = "row p-2" style = {{marginLeft: 40}}>
