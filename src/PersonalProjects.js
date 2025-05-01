9ed7d1371360467429eb9f951d5e0fab43fa3606
jordanxlau <jordanxlau@yahoo.ca>
2025-05-01 18:49:38 -0400

    update: inclusion of new personal projects

--- src/PersonalProjects.js
+++ src/PersonalProjects.js
@@ -10,7 +10,9 @@ function PersonalProjects({lang}) {
             <h2>{personal_projects}</h2>
             <p>{research_interests}</p>
             {/* Programming Projects */}
-            <p>Programming Projects (available on GitHub, <a class = "text-white" href ="https://github.com/jordanxlau" open>@jordanxlau</a>):
+            <p><b>Programming Projects (available on GitHub, <a class = "text-white" href ="https://github.com/jordanxlau" open>@jordanxlau</a>):</b>
+                <br />
+                <a class = "text-white" href="https://github.com/jordanxlau/comparison_of_language_pairs" open><u>Machine Translation for Different Language Pairs, 2025.</u></a>
                 <br /><br />
                 <a class = "text-white" href="https://github.com/jordanxlau/sign-language-detection" open><u>Hand Gesture Detection using Machine Learning, 2024.</u></a>
                 <br /><br />
@@ -27,14 +29,22 @@ function PersonalProjects({lang}) {
             <img class = "container-sm px-4" src = "../images/dbscan.png" style = {{height: '50%', width: '50%', objectFit: 'fill'}} alt=""></img>
             <p class = "px-4">A point scene captured by LiDAR, being clustered by the DBScan algorithm I implemented.</p>
             {/* Papers */}
-            <p>Papers:<br />
-                <a class = "text-white" href="../downloadables/Change in the Irish Palatalisation-Velarisation Contrast.pdf" open><u>Change in the Irish Palatalisation-Velarisation Contrast. 2024.</u></a>
+            <p><b>Papers:</b>
+                <br />
+                <a>Honours Project. 2025.</a>
                 <br /><br />
-                <a class = "text-white" href="../downloadables/Change in the Irish Palatalisation-Velarisation Contrast.pdf" open><u>Change in the Irish Palatalisation-Velarisation Contrast. 2024.</u></a>
+                <a class = "text-white" href="../downloadables/Machine Translation for Different Language Pairs.pdf" open><u>Machine Translation for Different Language Pairs, 2025.</u></a>
                 <br /><br />
-                <a class = "text-white" href="../downloadables/Word Order in Kabyle Free or Fixed.pdf" open><u>Word Order in Kabyle: Free or Fixed?. 2023.</u></a>
+                <a class = "text-white" href="../downloadables/Change in the Irish Palatalisation-Velarisation Contrast.pdf" open><u>Change in the Irish Palatalisation-Velarisation Contrast, 2024.</u></a>
+                <br /><br />
+                <a class = "text-white" href="../downloadables/Word Order in Kabyle Free or Fixed.pdf" open><u>Word Order in Kabyle: Free or Fixed?, 2023.</u></a>
                 <br /><br />
                 <a class = "text-white" href="../downloadables/The Ethical and Technological Challenges Facing Autonomous Driving.pdf" open><u>The Ethical and Technological Challenges Facing Autonomous Driving, 2021.</u></a>
+            </p>
+            {/* Presentations */}
+            <p><b>Presentations:</b>
+                <br />
+                <a>Discussion of a paper on Syntax Graph CNNs for Sentiment Analysis, 2025.</a>
             </p>
         </div>
     );
