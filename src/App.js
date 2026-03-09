11979177877a6944696b421b45c367299629b4d3
jordanxlau <jordanxlau@yahoo.ca>
2026-03-09 15:57:13 -0400

    refactor: code quality

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 function App({lang}) {
-  var english = lang === 'en';
+  let english = lang === 'en';
 
-  var all_about_me = english ? "Who Am I?" : "Qui Suis-Je?";
-  var intro = english ? "I recently finished my degree in Computer Science (with a minor in Linguistics) at uOttawa. I have done three internships as a software developer with the Government of Canada. I am a Dean's Honour List student and am proud to have been awarded the Merit Award Bursary and Canadian Hearing Services National Scholarship. My interests include swimming, karate, and languages."
+  let all_about_me = english ? "Who Am I?" : "Qui Suis-Je?";
+  let intro = english ? "I recently finished my degree in Computer Science (with a minor in Linguistics) at uOttawa. I have done three internships as a software developer with the Government of Canada. I am a Dean's Honour List student and am proud to have been awarded the Merit Award Bursary and Canadian Hearing Services National Scholarship. My interests include swimming, karate, and languages."
   : "J'ai récemment terminé mon B.Sc. spécialisé en Informatique (avec une mineure en Linguistique) à uOttawa. J'étais trois fois un stagiaire en développment de logiciels pour le Gouvernement Fédérale. Je suis membre des palmarès du doyen et fier d'avoir reçu la bourse « Merit Award Bursary » ainsi que La Bourse d'Études des SCO. Je pratique la natation et le karaté, et je m'intéresse aussi aux langues."
-  var this_website = english ? "This Website" : "Ce Site";
-  var this_website_explanation = english ? "This website showcases me as a student, programmer and tutor! I've written the whole site myself with React. It is hosted with GitHub Pages."
+  let this_website = english ? "This Website" : "Ce Site";
+  let this_website_explanation = english ? "This website showcases me as a student, programmer and tutor! I've written the whole site myself with React. It is hosted with GitHub Pages."
   : "Ce site me présente comme étudiant, tuteur et développeur de logiciels! Je l'ai fait moi-même avec React. Le site est publié sur GitHub Pages.";
-  var why_i_tutor = english ? "Why I Tutor" : "Pourquoi Je Suis un Tuteur";
-  var tutoring_intro = english ? "In over 6 years, I have tutored everything from 2nd-grade French Immersion to Calculus and Vectors. I have always been an avid learner, so I now enjoy sharing my knowledge with younger students to help them succeed as well."
+  let why_i_tutor = english ? "Why I Tutor" : "Pourquoi Je Suis un Tuteur";
+  let tutoring_intro = english ? "In over 6 years, I have tutored everything from 2nd-grade French Immersion to Calculus and Vectors. I have always been an avid learner, so I now enjoy sharing my knowledge with younger students to help them succeed as well."
   : "Dans plus que 6 ans, j'ai enseigné les élèves d'élémentaire jusqu'au lycée, en immersione française au calcul. Ayant toujours aimé apprendre, maintenant, j'aime partager mes connaissances avec mes élèves afin de les aider réussir.";
 
   return (
     <>
-      <div class = "container-fluid px-0 py-0 text-white bg-secondary text-left">
-          <div class = "row mx-0">
-              <div class = "col-md-6 px-0"><img class = "img-fluid" src = "images/jordan.jpg" alt=""></img></div>
-              <div class = "col-md-6 p-3">
+      <div className = "container-fluid px-0 py-0 text-white bg-secondary text-left">
+          <div className = "row mx-0">
+              <div className = "col-md-6 px-0"><img className = "img-fluid" src = "images/jordan.jpg" alt=""></img></div>
+              <div className = "col-md-6 p-3">
                   <h1><i><b>Jordan Lau</b></i></h1>
                   <h3>{this_website}</h3>
                   <p>{this_website_explanation}</p>
@@ -26,8 +26,8 @@ function App({lang}) {
                   <h3>{why_i_tutor}</h3>
                   <p>{tutoring_intro}</p>
                   <br />
-                  <a class = "btn btn-lg btn-outline-light" href = "downloadables/Jordan Lau Technical Resume.pdf" style={{margin: 3}} open>Developer Resume</a>
-                  <a class = "btn btn-lg btn-outline-light" href = "downloadables/Jordan Lau Teaching Resume.pdf" style={{margin: 3}} open>Tutor Resume</a>
+                  <a className = "btn btn-lg btn-outline-light" href = "downloadables/Jordan Lau Technical Resume.pdf" style={{margin: 3}} open>Developer Resume</a>
+                  <a className = "btn btn-lg btn-outline-light" href = "downloadables/Jordan Lau Teaching Resume.pdf" style={{margin: 3}} open>Tutor Resume</a>
               </div>
           </div>
       </div>
--- src/PersonalProjects.js
+++ src/PersonalProjects.js
@@ -1,49 +1,49 @@
 function PersonalProjects({lang}) {
-    var english = lang === 'en';
-    var personal_projects = english ? "Programming Projects" : "Projets de Programmation";
-    var research_interests = english ? "My interests include data science, applied and theoretical machine learning, natural language processing, audio processing and pure linguistics."
+    let english = lang === 'en';
+    let personal_projects = english ? "Programming Projects" : "Projets de Programmation";
+    let research_interests = english ? "My interests include data science, applied and theoretical machine learning, natural language processing, audio processing and pure linguistics."
     : "Je m'interesse en sciences de données, en aprentissage en machine, au traitement du langage naturel, au traitement audio et en linguistique théorique.";
 
 
     return (
-        <div>
-            <div class = "container-fluid p-3 text-white text-left" style = {{backgroundImage: 'url("../images/computer.jpg")', backgroundSize: 'cover'}}>
+        <>
+            <div className = "container-fluid p-3 text-white text-left" style = {{backgroundImage: 'url("../images/computer.jpg")', backgroundSize: 'cover'}}>
                 <h2>{personal_projects}</h2>
                 <p>{research_interests}</p>
-                <p>The projects I showcase below can be seen on my GitHub, <a class = "text-white" href ="https://github.com/jordanxlau" open>@jordanxlau</a>.
+                <p>The projects I showcase below can be seen on my GitHub, <a className = "text-white" href ="https://github.com/jordanxlau" open>@jordanxlau</a>.
                     <br /><br />
-                    <a class = "text-white" href="https://github.com/merlab/GONLP" open><u>Honours Project: Information Retrieval for Gene Ontology Annotation.</u></a>
+                    <a className = "text-white" href="https://github.com/merlab/GONLP" open><u>Honours Project: Information Retrieval for Gene Ontology Annotation.</u></a>
                     <br /><br />
-                    <a class = "text-white" href="https://github.com/jordanxlau/deepl-vs-google-translation" open><u>DeepL vs Google Translate Study.</u></a>
+                    <a className = "text-white" href="https://github.com/jordanxlau/deepl-vs-google-translation" open><u>DeepL vs Google Translate Study.</u></a>
                     <br /><br />
-                    <a class = "text-white" href="https://github.com/jordanxlau/sign-language-detection" open><u>Hand Gesture Detection using Machine Learning.</u></a>
+                    <a className = "text-white" href="https://github.com/jordanxlau/sign-language-detection" open><u>Hand Gesture Detection using Machine Learning.</u></a>
                     <br /><br />
-                    <a class = "text-white" href="https://github.com/CSI2132-G30/hotel" open><u>Hotel Management App.</u></a>
+                    <a className = "text-white" href="https://github.com/CSI2132-G30/hotel" open><u>Hotel Management App.</u></a>
                     <br /><br />
                     <a>Machine Translation for Different Language Pairs.</a>
                     <br /><br />
                     <a>Object Detection with DBScan.</a>
                 </p>
-                <img class = "container-sm px-4" src = "../images/dbscan.png" style = {{height: '50%', width: '50%', objectFit: 'fill'}} alt=""></img>
-                <p class = "px-4">A point scene captured by LiDAR, being clustered by the DBScan algorithm I implemented.</p>
+                <img className = "container-sm px-4" src = "../images/dbscan.png" style = {{height: '50%', width: '50%', objectFit: 'fill'}} alt=""></img>
+                <p className = "px-4">A point scene captured by LiDAR, being clustered by the DBScan algorithm I implemented.</p>
             </div>
-            <div class = "container-fluid p-3 bg-white text-left">
+            <div className = "container-fluid p-3 bg-white text-left">
                 <h2>Papers and Presentations</h2>
                 <p>
-                    <a class = "text-black" href="../downloadables/Machine Translation for Different Language Pairs.pdf" open><u>Machine Translation for Different Language Pairs. 2025.</u></a>
+                    <a className = "text-black" href="../downloadables/Machine Translation for Different Language Pairs.pdf" open><u>Machine Translation for Different Language Pairs. 2025.</u></a>
                     <br /><br />
-                    <a class = "text-black" href="../downloadables/Change in the Irish Palatalisation-Velarisation Contrast.pdf" open><u>Change in the Irish Palatalisation-Velarisation Contrast. 2024.</u></a>
+                    <a className = "text-black" href="../downloadables/Change in the Irish Palatalisation-Velarisation Contrast.pdf" open><u>Change in the Irish Palatalisation-Velarisation Contrast. 2024.</u></a>
                     <br /><br />
-                    <a class = "text-black" href="../downloadables/Word Order in Kabyle Free or Fixed.pdf" open><u>Word Order in Kabyle: Free or Fixed? 2023.</u></a>
+                    <a className = "text-black" href="../downloadables/Word Order in Kabyle Free or Fixed.pdf" open><u>Word Order in Kabyle: Free or Fixed? 2023.</u></a>
                     <br /><br />
-                    <a class = "text-black" href="../downloadables/The Ethical and Technological Challenges Facing Autonomous Driving.pdf" open><u>The Ethical and Technological Challenges Facing Autonomous Driving. 2022.</u></a>
+                    <a className = "text-black" href="../downloadables/The Ethical and Technological Challenges Facing Autonomous Driving.pdf" open><u>The Ethical and Technological Challenges Facing Autonomous Driving. 2022.</u></a>
                     <br /><br />
                     <a>Discussion of a paper on Syntax Graph CNNs for Sentiment Analysis. 2025.</a>
                 </p>
-                <iframe class = "px-4" width="560" height="315" src="https://www.youtube.com/embed/BFIjmmwBlT4?si=pIGabR8P_4FSqyid" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-                <p class = "px-4">In this video of the presentation, I am the third speaker.</p>
+                <iframe className = "px-4" width="560" height="315" src="https://www.youtube.com/embed/BFIjmmwBlT4?si=pIGabR8P_4FSqyid" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                <p className = "px-4">In this video of the presentation, I am the third speaker.</p>
             </div>
-        </div>
+        </>
     );
 }
 
